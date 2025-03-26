1defd15026dbc09d8e849738f0f06a85a60d874e
Niloufar <niloofarzahedi75@gmail.com>
2025-03-26 16:19:42 +0000

    generated empty controllers for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get "follows/create"
+  get "follows/destroy"
+  get "likes/create"
+  get "likes/destroy"
+  get "comments/create"
+  get "comments/destroy"
+  get "posts/index"
+  get "posts/show"
+  get "posts/new"
+  get "posts/create"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
   root 'pages#home'
   resources :articles
 end
