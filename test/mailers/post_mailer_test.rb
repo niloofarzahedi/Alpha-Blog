0f7496f91a9ccf0ef6aa9c291cc30a5d0e30be55
Niloufar <niloofarzahedi75@gmail.com>
2025-04-03 15:34:30 +0100

    added test for post back in stock notification with email

--- test/mailers/post_mailer_test.rb
+++ test/mailers/post_mailer_test.rb
@@ -1,11 +1,12 @@
 require "test_helper"
 
 class PostMailerTest < ActionMailer::TestCase
+  fixtures :posts, :subscribers
   test "in_stock" do
-    mail = PostMailer.in_stock
+    mail = PostMailer.with(post: posts(:post_one), subscriber: subscribers(:john_doe)).in_stock
     assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    assert_equal [ "john.doe@example.com" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Good news!", mail.body.encoded
   end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :posts, :users, :subscribers
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    #load a fixture post as an active record object
+    post = posts(:post_one)
+
+    # Set post out of stock
+    post.update(inventory_count: 0)
+
+    #beacause i only add two subscribers in fixtures i expect two emails to be sent
+    assert_emails 2 do
+      post.update(inventory_count: 99)
+    end
+  end
 end
